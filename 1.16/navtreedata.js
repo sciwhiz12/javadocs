42d58562097ce5233383899415917f03fe7458a2
sciwhiz12 <sciwhiz12@users.noreply.github.com>
2020-11-17 10:52:15 +0000

    Docs[1.16.x - 163880f1] Prepend brief desc. before detailed desc.

--- 1.16/navtreedata.js
+++ 1.16/navtreedata.js
@@ -40,7 +40,7 @@ var NAVTREE =
 
 var NAVTREEINDEX =
 [
-""
+"classnet_1_1minecraft_1_1client_1_1gui_1_1_abstract_gui.html#a68f8eeaf363746d678c9ac36c320f545"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
