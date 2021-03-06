6cca97b522af5738666ccd8f94f62b8ed60b532a
sciwhiz12 <sciwhiz12@users.noreply.github.com>
2021-03-06 07:43:48 +0000

    Docs[a4bee9f9@1.16.x] Add first batch of events

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.minecraftforge.versions.forge"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"net.minecraft.entity.passive"},{"l":"net.minecraftforge.client.event"},{"l":"net.minecraftforge.versions.forge"}];updateSearchResults();
